e98ddc698c2fe34d33371e2477dd8253cfff179f
brianasalagean <briana.salagean@stud.ubbcluj.ro>
2023-04-04 14:33:25 +0300

    lab5

---
+++ lab5x/Other/ManagerByAvgAgeSH.cs
@@ -0,0 +1,14 @@
+﻿namespace lab5.Other
+{
+    public class ManagerByAvgAgeSH
+    {
+        public int ManagerId { get; set; }
+        public int AvgAgeOfSuperHero { get; set; }
+
+        public ManagerByAvgAgeSH(int managerId, int avgAgeOfSuperHero)
+        {
+            ManagerId = managerId;
+            AvgAgeOfSuperHero = avgAgeOfSuperHero;
+        }
+    }
+}
